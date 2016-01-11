c4ec17cf3321177a5fc0d20083ec254f75997de9
fujiabin00 <13671669286@163.com>
2016-01-12 00:30:00 +0800

    log

--- js/domReady.js
+++ js/domReady.js
@@ -1,13 +1,1 @@
-//版权 北京智能社©, 保留所有权利
-function $(fn){
-	
-	if(document.addEventListener){//高级
-		document.addEventListener("DOMContentLoaded",fn,false);
-	} else {
-		document.attachEvent("onreadystatechange",function(){
-			if(document.readyState == "complete"){
-				fn &&fn();
-			}	
-		});
-	}
-}
+function $(fn){if(document.addEventListener){document.addEventListener("DOMContentLoaded",fn,false)}else{document.attachEvent("onreadystatechange",function(){if(document.readyState=="complete"){fn&&fn()}})}};
