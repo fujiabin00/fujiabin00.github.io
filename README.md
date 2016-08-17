2bd03bccd354a0ff07d3ffb38872579ccb9e30ef
fujiabin <fu.jiabin@xiaoyi.com>
2016-08-17 16:33:53 +0800

    meteor

--- README.md
+++
@@ -1,6 +0,0 @@
-# fujiabin00.github.com
-<<<<<<< HEAD
-第一个文件
-=======
-个人作品
->>>>>>> 7e49e03bd63561bfa6fc77882c4d63e03e1a4429
--- js/domReady.js
+++
@@ -1,1 +0,0 @@
-function $(fn){if(document.addEventListener){document.addEventListener("DOMContentLoaded",fn,false)}else{document.attachEvent("onreadystatechange",function(){if(document.readyState=="complete"){fn&&fn()}})}};
